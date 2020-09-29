15e08befb564248fee70a983254d96256b179e24
Jeff Neuberger <jeffreyneuberger@gmail.com>
2020-09-29 10:41:26 -0400

    feat: add image and style drop spot

--- src/components/ImageChooser.js
+++ src/components/ImageChooser.js
@@ -2,12 +2,21 @@ import React from 'react';
 
 function ImageChooser() {
   return (
-    <div className="flex flex-col items-center justify-center border-solid rounded-md shadow-xl p-10">
-      <h1 className="text-2xl m-5">Upload your image</h1>
-      <p className="text-md m-5">File should be Jpeg, Png...</p>
-      <div className="m-5">IMG HOLDER</div>
-      <p className="text-lg text-gray-600 my-5">Or</p>
-      <button className="rounded-lg bg-blue-600 text-white py-2 px-4 my-5">
+    <div className="flex flex-col items-center justify-center rounded-lg shadow-2xl p-5 py-10 w-1/2">
+      <h1 className="text-xl m-2">Upload your image</h1>
+      <p className="text-xs m-2">File should be Jpeg, Png...</p>
+      <div className="bg-indigo-100 border-2 border-dashed border-indigo-200 rounded-lg m-2 w-full flex flex-col items-center">
+        <img
+          src="/upload-holder.svg"
+          alt="gray mountains with blue moon in background"
+          className="m-10"
+        />
+        <p className="text-sm mb-10 text-gray-600">
+          Drag & Drop your image here
+        </p>
+      </div>
+      <p className="text-sm text-gray-600 my-5">Or</p>
+      <button className="rounded-lg bg-indigo-600 text-white py-2 px-6 my-5">
         Choose a file
       </button>
     </div>
