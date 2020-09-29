a16f0d7b44b7e8fea12297c3fb6f014111701706
Jeff Neuberger <jeffreyneuberger@gmail.com>
2020-09-29 10:20:04 -0400

    feat: create imagechooser layout

--- src/components/ImageChooser.js
+++ src/components/ImageChooser.js
@@ -2,8 +2,14 @@ import React from 'react';
 
 function ImageChooser() {
   return (
-    <div className="flex items-center justify-center">
-      <h1 className="text-2xl">Image Chooser</h1>
+    <div className="flex flex-col items-center justify-center border-solid rounded-md shadow-xl p-10">
+      <h1 className="text-2xl m-5">Upload your image</h1>
+      <p className="text-md m-5">File should be Jpeg, Png...</p>
+      <div className="m-5">IMG HOLDER</div>
+      <p className="text-lg text-gray-600 my-5">Or</p>
+      <button className="rounded-lg bg-blue-600 text-white py-2 px-4 my-5">
+        Choose a file
+      </button>
     </div>
   );
 }
