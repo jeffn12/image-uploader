7ce761c7ce883a2d50d775b668b5f9fe25383ff2
Jeff Neuberger <jeffreyneuberger@gmail.com>
2020-09-29 15:02:57 -0400

    feat: define user story tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('User Stories', () => {
+  test('User story: I can drag and drop an image to upload it', () => {});
+  test('User story: I can choose to select an image from my folder', () => {});
+  test('User story: I can see a loader when uploading', () => {});
+  test('User story: When the image is uploaded, I can see the image and copy it', () => {});
+  test('User story: I can choose to copy to clipboard', () => {});
 });
