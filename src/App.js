3b7b23cfd642e01e34ecf764b986e14da5a170d9
Jeff Neuberger <jeffreyneuberger@gmail.com>
2020-10-07 17:08:33 -0400

    feat: conditional render components on state

--- src/App.js
+++ src/App.js
@@ -6,13 +6,13 @@ import Loading from './components/Loading';
 import ResponseScreen from './components/ResponseScreen';
 
 function App() {
-  const [files, setFiles] = useState([]);
+  const [files, setFiles] = useState(null);
   const [loading, setLoading] = useState(false);
   const [url, setURL] = useState('');
 
   return (
     <div className="flex flex-col items-center justify-center w-screen h-screen">
-      <ImageChooser files={files} setFiles={setFiles} />
+      {!files && <ImageChooser files={files} setFiles={setFiles} />}
       {loading && <Loading />}
       {url && <ResponseScreen />}
     </div>
