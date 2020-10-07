f165e9832679cc0c096ea862637966548b99046d
Jeff Neuberger <jeffreyneuberger@gmail.com>
2020-10-07 18:30:30 -0400

    refactor: fetch to axios

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
-/* eslint-disable-next-line no-unused-vars*/
+import axios from 'axios';
+// Components
 import ImageChooser from './components/ImageChooser';
-/* eslint-disable-next-line no-unused-vars*/
 import Loading from './components/Loading';
 import ResponseScreen from './components/ResponseScreen';
 
@@ -14,21 +14,17 @@ function App() {
       console.log(files);
       if (files[0].type.startsWith('image')) {
         // send img info to API, get back signed URL
-        fetch(
-          'https://m4xen5xkpf.execute-api.us-east-1.amazonaws.com/dev/api/getPreSignedURL',
-          {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
+        axios
+          .post(
+            'https://m4xen5xkpf.execute-api.us-east-1.amazonaws.com/dev/api/getPreSignedURL',
+            {
               fileName: files[0].name,
               fileType: files[0].type,
-            }),
-          }
-        )
-          .then((res) => res.json())
-          .then(({ uploadURL }) => {
+            }
+          )
+          .then((res) => {
+            console.log(res);
+            const { uploadURL } = res.data;
             console.log(uploadURL);
             // use signed URL as endpoint to send img
             // setURL(return value)
