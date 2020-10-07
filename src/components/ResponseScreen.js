5ad6bb8f52170808c5a529f32ad0f51540d1e2cc
Jeff Neuberger <jeffreyneuberger@gmail.com>
2020-10-07 19:42:22 -0400

    feat: format text in link display

--- src/components/ResponseScreen.js
+++ src/components/ResponseScreen.js
@@ -17,7 +17,8 @@ function ResponseScreen(props) {
         />
       </div>
       <div className="flex items-center border-2 bg-indigo-100 border-indigo-200 h-12 w-full rounded-lg m-1">
-        <p className="bg-indigo-100 m-2 w-3/4 text-xs">{props.url}</p>
+        <p className="bg-indigo-100 m-2 w-3/4 text-xs truncate">{props.url}</p>
+        {/** TODO: add more visual confirmation on button press */}
         <button
           className="rounded-lg bg-indigo-600 text-white focus:outline-none focus:bg-indigo-700 text-xs w-1/3 h-full hover:bg-indigo-800"
           onClick={(e) => {
